6c2c92109d75800d0a639eb4bfd9e4aeb4b2ba62
JeraldJF <jeraldj@sanketika.in>
2025-05-07 17:59:18 +0530

    feat #FP-I26: File name change

--- src/config/assessmentConfig.ts
+++ src/config/assessmentConfig.ts
@@ -1,5 +1,5 @@
 export const assessmentConfig = {
-    csvPath: process.env.CSV_PATH || './data/assessment_create.csv',
+    csvPath: process.env.CSV_PATH || './data/assessment.csv',
     questionCsvPath: process.env.QUESTION_CSV_PATH || './data/questions.csv',
     createdBy: process.env.CREATED_BY || '927c2094-987f-4e8f-8bd5-8bf93e3d2e8a',
     organisation: process.env.ORGANISATION ? [process.env.ORGANISATION] : ['Fmps'],
