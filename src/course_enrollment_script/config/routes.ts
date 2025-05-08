f215818868029665a14b1a879e9d2ca5cd888b1f
Harish Kumar Gangula <harish@sanketika.in>
2025-05-08 20:24:41 +0530

    Initial commit

---
+++ src/course_enrollment_script/config/routes.ts
@@ -0,0 +1,9 @@
+export const routes = {
+    listBatch: '/api/course/v1/batch/list',
+    createLearnerProfile: '/api/collection/v1/create',
+    searchCourse: '/api/composite/v1/search',
+    enrollUser: '/api/course/v1/enrol',
+    updateLearnerProfile: '/api/collection/v1/hierarchy/update',
+    getToken: '/auth/v1/refresh/token',
+    getRefeshToken: '/auth/realms/sunbird/protocol/openid-connect/token',
+};
---
+++ src/globalConfigs.ts
@@ -0,0 +1,18 @@
+const globalConfig = {
+    baseUrl: process.env.BASE_URL || 'https://dev.sunbirded.org',
+    apiAuthKey: process.env.AUTH_KEY || '',
+    username: process.env.USERNAME || '',
+    password: process.env.PASSWORD || '',
+    userToken: process.env.TOKEN || '',
+    clientId: process.env.CLIENT_ID || '',
+    clientSecret: process.env.CLIENT_SECRET || '',
+    grant_type: process.env.GRANT_TYPE || 'password',
+    channelId: process.env.CHANNEL_ID || '01429195271738982411',
+    createdBy: process.env.CREATED_BY || '927c2094-987f-4e8f-8bd5-8bf93e3d2e8a',
+    organisation: process.env.ORGANISATION ? [process.env.ORGANISATION] : ['FMPS Org'],
+    framework: process.env.FRAMEWORK || 'FMPS',
+    mimeType: process.env.MIME_TYPE || 'application/vnd.ekstep.ecml-archive',
+    creator: process.env.CREATOR || 'Content Creator FMPS',
+}
+
+export default globalConfig;
